e9d366a5af2706ae64d5d53c28f8a8d5b37714f2
Michael D Hale <mduaneh@gmail.com>
2020-02-13 17:49:45 -0500

    Making updates

--- transferLearning/runDocker.sh
+++ transferLearning/runDocker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 docker run --rm -it \
-	--runtime=nvidia \
-	-p 5000:5000 \
-	-v $PWD'/PowerCell:/sly_task_data/model' \
+	-v $PWD:/sly_task_data/model' \
 	supervisely/nn-tf-obj-det:latest \
 	"$@"
-	python /workdir/src/rest_inference.pysupervisely/nn-tf-obj-det:latest
