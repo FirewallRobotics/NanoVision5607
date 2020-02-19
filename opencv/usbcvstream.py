e67ce4dedf886cd455ba13a832a594802cf73a57
abby blancett <abbyblancett@gmail.com>
2020-02-18 19:42:27 -0500

    Working on 20200217

--- opencv/usbcvstream.py
+++ opencv/usbcvstream.py
@@ -10,6 +10,7 @@ import cscore as cs
 import numpy as np
 import cv2
 from powercellcv import *
+from networktables import NetworkTables
 
 def main():
     hue = [9.712230215827338, 57.82664451107176]
@@ -20,17 +21,17 @@ def main():
     blur_ksize = int(2 * round(blur_radius) + 1)
     powercell = powercellcv()
     camera = cs.UsbCamera("usbcam", 1)
-    camera.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
+    camera.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 15)
 
-    mjpegServer = cs.MjpegServer("httpserver", 8081)
-    mjpegServer.setSource(camera)
+   # mjpegServer = cs.MjpegServer("httpserver", 8081)
+   # mjpegServer.setSource(camera)
 
-    print("mjpg server listening at http://0.0.0.0:8081")
+   # print("mjpg server listening at http://0.0.0.0:8081")
 
     cvsink = cs.CvSink("cvsink")
     cvsink.setSource(camera)
 
-    cvSource = cs.CvSource("cvsource", cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
+    cvSource = cs.CvSource("cvsource", cs.VideoMode.PixelFormat.kMJPEG, 320, 240, 15)
     cvMjpegServer = cs.MjpegServer("cvhttpserver", 8082)
     cvMjpegServer.setSource(cvSource)
 
@@ -44,7 +45,31 @@ def main():
     iterations = 1.0
     bordertype = cv2.BORDER_CONSTANT
     bordervalue = (-1)
+    sd = NetworkTables.getTable("powerball")
     
+
+    #=====
+    NetworkTables.initialize(server='roborio-5607-frc.local')
+  # cs2 = cs2.getInstance()
+  # cs2.enableLogging()
+
+  # camera2 = cs2.startAutomaticCapture()
+
+  # camera2.setResolution(320, 240)
+
+  # # Get a CvSink. This will capture images from the camera
+  # cvSink2 = cs2.getVideo()
+
+  # # (optional) Setup a CvSource. This will send images back to the Dashboard
+  # outputStream2 = cs2.putVideo("Rectangle", 320, 240)
+
+  # # Allocating new images is very expensive, always try to preallocate
+  # img2 = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+
+  # while True:
+  #     # Tell the CvSink to grab a frame from the camera and put it
+  #     # in the source image.  If there is an error notify the output.
+  #     time, img = cvSink.grabFrame(img)
 
     while True:
 
@@ -63,6 +88,9 @@ def main():
         out = cv2.erode(out, kernel, anchor, iterations = 2)#, cv2.BORDER_CONSTANT , bordervalue)
         cnts, a = cv2.findContours(out, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         center = None
+
+
+
         #only do stuff if a single contor was found
         if len(cnts) > 0:
             #find the largest contour in the mask, then use it
@@ -81,22 +109,24 @@ def main():
                 #draw a circle around the target and publish values to smart dashboard
                 cv2.circle(frame, (int(x), int(y)), int(radius), (255,255,8), 2)
                 cv2.circle(frame, center, 3, (0,0,225), -1)
-                #sd.putNumber('X',x)
-                #sd.putNumber('Y',y)
-                #sd.putNumber('R', radius)
+                sd.putNumber('X',x)
+                sd.putNumber('Y',y)
+                sd.putNumber('R', radius)
                 print("X: " + repr(round(x, 1)) + " Y: " + repr(round(y, 1)) + " Radius: " + repr(round(radius, 1)))
 
             else:
                 print("WTF")
                 #let the RoboRio Know no target has been detected with -1
-                #sd.putNumber('X', -1)
-                #sd.putNumber('Y', -1)
-                #sd.putNumber('R', -1)
+                sd.putNumber('X', -1)
+                sd.putNumber('Y', -1)
+                sd.putNumber('R', -1)
 
         #powercell.process(test)
         cvSource.putFrame(frame)
-
+      
 
 
 if __name__ == "__main__":
     main()
+
+
