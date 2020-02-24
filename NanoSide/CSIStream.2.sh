3917dae142b33b9c990888860e4b91314e4b1346
Drive Team 5607 <driveteam5607@firewallrobotics.org>
2020-02-23 19:34:24 -0500

    Not working

--- NanoSide/CSIStream.2.sh
+++ NanoSide/CSIStream.2.sh
@@ -19,13 +19,14 @@ done
 
 #two arguments: client IP, client port
 #1280 x 720
-gst-launch-1.0 --gst-debug-level=2  nvarguscamerasrc !\
-	'video/x-raw(memory:NVMM),width=3820, height=2464, framerate=21/1, format=NV12' !\
-	        nvvidconv flip-method=2 ! omxh264enc bitrate=600000  ! 'video/x-h264, stream-format=(string)byte-stream' !\
-	        h264parse ! rtph264pay ! udpsink host=$host port=$port
-exit
 gst-launch-1.0 nvarguscamerasrc !\
 'video/x-raw(memory:NVMM),width=640, height=360, framerate=30/1, format=NV12' !\
           nvvidconv flip-method=2 ! omxh264enc bitrate=3000000 control-rate=4 ! 'video/x-h264, stream-format=(string)byte-stream' !\
        h264parse !  mpegtsmux ! rtpmp2tpay ! udpsink async=false host=$host port=$port
+exit
+#!  mpegtsmux ! rtpmp2tpay ! udpsink async=false host=$host port=$port
 # rtph264pay
+gst-launch-1.0 nvarguscamerasrc ! \
+	  'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, \
+	    format=(string)NV12, framerate=(fraction)15/1' ! nvv4l2h264enc ! \
+	      h264parse !mpegtsmux ! rtpmp2tpay ! udpsink async=false host=$host port=$port
