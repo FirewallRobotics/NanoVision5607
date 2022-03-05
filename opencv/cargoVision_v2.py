67aa74326d109d02a03c01d2d99b5de99c131c45
abbyblancett <59714313+abbyblancett@users.noreply.github.com>
2022-03-05 14:28:02 -0500

    Yay

--- opencv/cargoVision_v2.py
+++ opencv/cargoVision_v2.py
@@ -14,12 +14,13 @@ def cargoProcess(frame, hue, sat, val):
     kernel = None
     anchor = (-1, -1)
     out = cv2.inRange(frame, (hue[0], sat[0], val[0]),  (hue[1], sat[1], val[1]))
-    out = cv2.dilate(out, kernel, anchor, iterations = 3)#, cv2.BORDER_CONSTANT , bordervalue)########
-    out = cv2.erode(out, kernel, anchor, iterations = 2)#, cv2.BORDER_CONSTANT , bordervalue)
+    #out = cv2.erode(frame, kernel, anchor, iterations = 2)
+    #out = cv2.dilate(out, kernel, anchor, iterations = 3)#, cv2.BORDER_CONSTANT , bordervalue)########
+    #out = cv2.erode(out, kernel, anchor, iterations = 2)#, cv2.BORDER_CONSTANT , bordervalue)
 
     cnts, a = cv2.findContours(out, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    center = None
-
+    center = [0,0]
+    data = [ [0,0], 0]
     #only do stuff if a single contor was found
     if len(cnts) > 0:
         #find the largest contour in the mask, then use it
@@ -36,7 +37,8 @@ def cargoProcess(frame, hue, sat, val):
             center = [0,0]
             data = [center, 0]
         
-        return data
+    data.append(out)   
+    return data
         
 class _team5607_NetworkTables:
     def __init__(self, server="roborio-5607-frc.local", tableName="cargo", tableKeys=[]):
@@ -93,19 +95,33 @@ def main():
     HEIGHT=90*SCALE
     # 160x90 is the smallest, we can use SCALE to pick larger sizes
     FPS=15
-    cvsink, cvSource = setupSinkSource(0,"Cargo", 8082, WIDTH, HEIGHT, FPS)
+    ##cvsink, cvSource = setupSinkSource(0,"Cargo", 8082, WIDTH, HEIGHT, FPS)
+    camera = cs.UsbCamera("usbcam", 0)#1, devcam or vid
+    camera.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, WIDTH, HEIGHT, FPS)
+
+    cvsink = cs.CvSink("cvsink")
+    cvsink.setSource(camera)
+
+    cvSource = cs.CvSource("cvsource", cs.VideoMode.PixelFormat.kMJPEG, WIDTH, HEIGHT, FPS) #get rid of red by nanovision code
+    cvSourceMid = cs.CvSource("cvsource", cs.VideoMode.PixelFormat.kMJPEG, WIDTH, HEIGHT, FPS) #get rid of red by nanovision code
+
+    cvMjpegServerMid = cs.MjpegServer("PowerCell", 8082)#here
+    cvMjpegServerMid.setSource(cvSourceMid)
+
+    cvMjpegServer = cs.MjpegServer("PowerCell", 8081)#here
+    cvMjpegServer.setSource(cvSource)
     cargo={
-        "blue": {
-            "hue": [92, 115],
-            "sat": [126, 235],
-            "val": [36, 183],
-            "color": (0,0,255) # RGB
-        },
+      "blue": {
+          "hue": [92, 110],
+          "sat": [108, 230],
+          "val": [48, 255],
+          "color": (255,0,0) # RGB
+      },
         "red":  {
-            "hue":  [0, 180],
-            "sat": [137, 223.0],
-            "val": [127, 255.0],
-            "color": (255,0,0) # RGB
+            "hue":  [147, 180],
+            "sat": [113, 230.0],
+            "val": [175, 255.0],
+            "color": (0,0,255) # RGB
         }
     }
 
@@ -118,20 +134,28 @@ def main():
     # Move network tables complexity to a single call
     team5607_cargo=_team5607_NetworkTables(server='roborio-5607-frc.local', tableName="cargo", tableKeys=tableKeys)
     # Start capturing video
+    count = 0
     while True:
         time, frame = cvsink.grabFrame(np.zeros(shape=(HEIGHT, WIDTH, 3), dtype=np.uint8))
         if time == 0:
             print("error:", cvsink.getError())
             continue
+        count+=1
         image=frame.copy()
         image=preprocess(image)
+        if count == 10:
+            cv2.imwrite('source.jpg', frame)
+        
         for label in cargo.keys():
                 # We only want to get the x,y,r based on a clean preprocessed image ONLY.
-                centers, rad = cargoProcess(image, hue=cargo[label]["hue"],sat=cargo[label]["sat"],val=cargo[label]["val"],)
+                data = cargoProcess(image, hue=cargo[label]["hue"],sat=cargo[label]["sat"],val=cargo[label]["val"])
+                centers = data[0]
+                rad = data[1]
                 # Now draw circles on the original image
                 frame = drawCircle(frame, centers, rad, color=cargo[label]["color"], minRadius = 7)
                 team5607_cargo.updateTable({f"X_{label}":centers[0], f"Y_{label}":centers[1], f"R_{label}":rad})
 
+        cvSource.putFrame(frame)
 
 
 if __name__ == "__main__":
