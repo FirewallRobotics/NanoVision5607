79bc731513e091118f33ee79c7dda023f1919864
Wendy Darby <32006906+Wendydarby@users.noreply.github.com>
2023-03-11 14:59:29 -0500

    debug

--- opencv/aprilTags.py
+++ opencv/aprilTags.py
@@ -1,12 +1,14 @@
 import apriltag
 import argparse
-import cv2
+import cv2 
+import numpy as np
 from networktables import NetworkTables
-import AprilTagPoseEstimator
-import Transform3d
-import Rotation3d
+from cscore import CameraServer
+# import AprilTagPoseEstimator
+#import Transform3d
+#import Rotation3d
 import glob
-
+'''
 def area(ptA, ptB, ptC, ptD):
   """Finds the area of the apriltag.
   args:
@@ -18,19 +20,43 @@ def area(ptA, ptB, ptC, ptD):
   length = ptB[0] - ptA[0]
   width = ptB[1] - ptC[1]
   area = length * width
-  return area
+  return area'''
   
 # construct the argument parser and parse the arguments
-
+#def main():
 ####thinking here
-cameras = {
+'''cameras = {
         "apriltag": "/dev/v4l/by-id/usb-EMEET_HD_Webcam_eMeet_C960_SN0001-video-index0",
         "items": "/dev/v4l/by-id/usb-Microsoft_Microsoft®_LifeCam_HD-3000-video-index0"
     }
-camera = cs.UsbCamera("usbcam", cameras["apriltag"])#1, devcam or vid
-camera.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, WIDTH, HEIGHT, FPS)
+   camera = cs.UsbCamera("usbcam", cameras["apriltag"])#1, devcam or vid'''
+vid = cv2.VideoCapture(0)
+cs = CameraServer
+cs.enableLogging()
+# cs.UsbCamera("usbcam", "0")
+camera = cs.startAutomaticCapture()
+cvsink = cs.getVideo()
 
-cvsink = cs.CvSink("cvsink")
+# (optional) Setup a CvSource. This will send images back to the Dashboard
+#outputStream = cs.putVideo("Name", 320, 240)
+
+# camera.setVideoMode(cs.VideoMode.PixelFormat.kMJPEG, WIDTH, HEIGHT, FPS)
+# Allocating new images is very expensive, always try to preallocate
+img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+while True:
+    # Tell the CvSink to grab a frame from the camera and put it
+    # in the source image. If there is an error notify the output. time, img = cvSink.grabFrame(img)
+    time, img = cvsink.grabFrame(img)
+    if time == 0:
+        # Send the output the error.
+      # skip the rest of the current iteration continue
+      #outputStream.notifyError(cvsink.getError())
+    #
+    # Insert your image processing logic here!
+    #
+    # (optional) send some image back to the dashboard
+outputStream.putFrame(img)
+'''cvsink = cs.CvSink(ImageSink(VideoSink.kMjpeg),VideoSink())
 cvsink.setSource(camera)
 
 cvSource = cs.CvSource("cvsource", cs.VideoMode.PixelFormat.kMJPEG, WIDTH, HEIGHT, FPS) #get rid of red by nanovision code
@@ -71,21 +97,21 @@ imgpoints = [] # 2d points in image plane.
 images = glob.glob('*.png')
 
 for image in images:
-  img = cv.imread(image)
-  gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  img = cv2.imread(image)
+  gray = cv2.cvtColor(img, cv.COLOR_BGR2GRAY)
   
   #Find the chess board corners
-  ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+  ret, corners = cv2.findChessboardCorners(gray, chessboardSize, None)
   
   if ret == True:
     objpoints.append(objp)
-    corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+    corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
     # Draw and display the corners
-    cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
-    cv.imshow('img',img)
-    cv.waitKey(1000)
+    cv2.drawChessboardCorners(img, chessboardSize, corners2, ret)
+    cv2.imshow('img',img)
+    cv2.waitKey(1000)
     
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
 
 ############## CALIBRATION #######################################################
 
@@ -93,24 +119,24 @@ ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints,
 
 ############## UNDISTORTION ######################################################
 
-img = cv.imread('cali5.png')
+img = cv2.imread('cali5.png')
 h, w = img.shape[:2]
-newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
 
 # Undistort
-dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix
+dst = cv2.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
 
 # crop the image
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
-cv.imwrite('caliResult2.png', dst)
+cv2.imwrite('caliResult2.png', dst)
                    
 # Reprojection Error
 mean_error = 0
                    
 for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
     mean_error += error
 
 print( "total error: {}".format(mean_error/len(objpoints)) )
@@ -120,26 +146,26 @@ print( "total error: {}".format(mean_error/len(objpoints)) )
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
   help="path to input image containing AprilTag")
- args = vars(ap.parse_args())
+args = vars(ap.parse_args())
  
  # load the input image and convert it to grayscale
- print("[INFO] loading image..."
- image = cv2.imread(args["image"])
- gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+print("[INFO] loading image...")
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  
  # define the AprilTags detector options and then detect the AprilTags
  # in the input image
- print("[INFO] detecting AprilTags...")
- options = apriltag.DetectorOptions(families="tag16h5")
- detector = apriltag.Detector(options)
- results = detector.detect(gray)
- print("[INFO] {} total AprilTags detected".format(len(results)))
+print("[INFO] detecting AprilTags...")
+options = apriltag.DetectorOptions(families="tag16h5")
+detector = apriltag.Detector(options)
+results = detector.detect(gray)
+print("[INFO] {} total AprilTags detected".format(len(results)))
        
-poseEstConfig = AprilTagPoseEstimator.Config(0,0,0,0,0) #need numbers
-estimator = AprilTagPoseEstimator(poseEstConfig)
+##poseEstConfig = AprilTagPoseEstimator.Config(0,0,0,0,0) #need numbers
+#e#stimator = AprilTagPoseEstimator(poseEstConfig)
  
  # loop over the AprilTag detection results
- for r in results:
+for r in results:
   # extract the bounding box (x, y)-coordinates for the AprilTag
   # and convert each of the (x, y)-coordinate pairs to integers
   (ptA, ptB, ptC, ptD) = r.corners
@@ -150,10 +176,10 @@ estimator = AprilTagPoseEstimator(poseEstConfig)
   
   
   # draw the bounding box of the AprilTag detection
-  cv2.line(image, ptA, ptB, [0, 255, 0), 2]
-  cv2.line(image, ptB, ptC, [0, 255, 0), 2]
-  cv2.line(image, ptC, ptD, [0, 255, 0), 2]
-  cv2.line(image, ptD, ptA, [0, 255, 0), 2]
+  cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+  cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+  cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+  cv2.line(image, ptD, ptA, (0, 255, 0), 2)
   
   # draw the center (x, y)-coordinates of the AprilTag
   (cX, cY) = (int(r.center[0]), int(r.center[1]))
@@ -171,12 +197,12 @@ estimator = AprilTagPoseEstimator(poseEstConfig)
    
            
 # show the output image after AprilTag detection
- NetworkTables.initialize(server='roborio-5607-frc.local')
- sd1 = NetworkTables.getTable("apriltag")
- sd1.putNumber('Point A', ptA)  ## tuple
- sd1.putNumber('Point B', ptB) #tuple
- sd1.putNumber('Point C', ptC)
- sd1.putNumber('Point D', ptD)
- sd1.putNumber('Center X', cX)
- sd1.putNumber('Center Y', cY)
- sd1.putNumber('ID', ID)
+NetworkTables.initialize(server='roborio-5607-frc.local')
+sd1 = NetworkTables.getTable("apriltag")
+sd1.putNumber('Point A', ptA)  ## tuple
+sd1.putNumber('Point B', ptB) #tuple
+sd1.putNumber('Point C', ptC)
+sd1.putNumber('Point D', ptD)
+sd1.putNumber('Center X', cX)
+sd1.putNumber('Center Y', cY)
+sd1.putNumber('ID', ID)'''
